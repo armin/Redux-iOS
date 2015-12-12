a048dbbe648d099de5090a586d1607b5d78d78d1
Armin Kroll <armin@jtribe.com.au>
2015-12-12 23:10:21 +1100

    Initial Commit

---
+++ ReduxApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ReduxApp
+//
+//  Created by Armin Kroll on 12/12/2015.
+//  Copyright © 2015 jtribe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
